3332f34982274c5d0958aef7c91b5fb1813fc65c
brunohhomem <brunohhomem@gmail.com>
2025-05-28 13:29:57 -0300

    feat(auth): implement session management and redirect pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,24 @@
-const DashboardPage = () => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session?.user) redirect("/authentication")
+
   return (
     <div>
-      <h4>
+      <h1>
         Dashboard
-      </h4>
+      </h1>
+      <h2>{session?.user.name}</h2>
+      <SignOutButton />
     </div>
   );
 }
